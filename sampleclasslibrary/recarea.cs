f7679b5c124c4731ce103fb135f00a8b4eaf162a
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:38:10 +0530

    Initial commit

---
+++ sampleclasslibrary/recarea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleclasslibrary
+{
+    public class recarea
+    {
+        public int length { get; set; }
+
+        public int breadth { get; set; }
+
+        public string Area()
+        {
+            return string.Format("area={0}", length * breadth);
+
+        }
+    }
+}
